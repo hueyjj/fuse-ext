0d6c86032f531a3754186cf550042dcd8a8133ad
hueyjj <jasperjeng@gmail.com>
2018-07-11 01:44:18 -0700

    Moved chrome-ext to fuse-client repository

--- chrome-ext/youtube-dl-ext/popup.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Close tab when after magnet link has opened
-    chrome.tabs.onCreated.addListener(function(tab) {
-        chrome.tabs.remove(tab.id);
-    });
-   
-});
